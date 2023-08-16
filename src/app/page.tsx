a920c44499eed5915a57e56e30a905a5f37b45d0
mgavillo <marie.gavillon@gmail.com>
2023-08-16 16:25:03 +0100

    first iteration with rnext router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@
 import Arrow from "@/components/Arrow";
 import "./globals.css";
 import Image from "next/image";
-import { useRef, useState } from "react";
+import { EventHandler, useEffect, useRef, useState } from "react";
 import { FiExternalLink, FiLink2, FiHeart, FiXCircle } from "react-icons/fi";
 import items from "./data";
 import Points from "./Points";
 import ExternalLink from "./ExternalLink";
 const INDENTATION_PX = 30;
+import { useRouter, useSearchParams } from "next/navigation";
 
 function Linkage({ points = 0 }: { points?: number }) {
   return (
@@ -21,12 +22,26 @@ function Linkage({ points = 0 }: { points?: number }) {
   );
 }
 
-function AccordionComponent({ level, e, parent }: { level: number; e: any; parent: string | undefined }) {
+function AccordionComponent({
+  level,
+  e,
+  parent,
+  setHistoricalItems,
+}: {
+  level: number;
+  e: any;
+  parent: string | undefined;
+  setHistoricalItems: React.Dispatch<React.SetStateAction<string[] | undefined>>;
+}) {
   const [children, setChildren] = useState<any[]>(e.children);
   const detailsRef = useRef<HTMLDetailsElement>(null);
   const [detailsOpened, setDetailsOpened] = useState<boolean>(false);
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-  // children?.map((e:any) => console.log(e))
+  useEffect(() => {
+    setChildren(e.children);
+  }, [e]);
   function summaryClick(e: React.MouseEvent) {
     e.preventDefault();
   }
@@ -36,7 +51,11 @@ function AccordionComponent({ level, e, parent }: { level: number; e: any; paren
     detailsRef.current.open = !detailsRef.current.open;
     setDetailsOpened(detailsRef.current.open);
   }
-  const onNegate = () => {
+  const onNegate = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    e.preventDefault();
+    e.nativeEvent.stopImmediatePropagation();
+    e.nativeEvent.stopPropagation();
     if (!detailsRef.current) return;
     detailsRef.current.open = true;
     setDetailsOpened(detailsRef.current.open);
@@ -46,15 +65,42 @@ function AccordionComponent({ level, e, parent }: { level: number; e: any; paren
     _children.map((e: any) => console.log(e));
     setChildren(_children);
   };
+
   const paddingLeft = `${INDENTATION_PX * level + (children ? 6 : 60)}px`;
   const claimStyle = `relative flex justify-between items-center gap-4 font-medium cursor-pointer list-none border border-transparent px-5 py-4 hover:border-slate-500 rounded-md ${
     level % 2 ? "bg-slate-200" : " bg-slate-100"
   }`;
 
+  function newRoute() {
+    const current = searchParams.get("id");
+    // const route = ""
+    if (current?.includes(e.id)) return;
+    const route = current ? `${e.id},${current}` : e.id;
+    router.push(`?id=${route}`);
+    // router.replace(`?id=${e.id}`)
+  }
+
+  function handleClick(e: React.MouseEvent) {
+    e.stopPropagation();
+    const selection = window.getSelection()?.toString();
+    if (selection) return;
+    expandDetails();
+  }
+
+  function handleDoubleClick(e: React.MouseEvent) {
+    e.stopPropagation();
+    e.preventDefault();
+    newRoute();
+  }
+
+  function toggle(e: any) {
+    e.preventDefault();
+  }
   if (e.input)
     return (
       <div
         className={claimStyle + " flex flex-col order-first border-2 border-black"}
+        onClick={(e) => e.stopPropagation}
         style={{ paddingLeft: paddingLeft }}>
         <textarea placeholder={"The claim `" + parent + "` is not true because ..."} className="w-full h-36" />
         <div className="w-full flex gap-2 justify-end">
@@ -64,10 +110,16 @@ function AccordionComponent({ level, e, parent }: { level: number; e: any; paren
       </div>
     );
   return (
-    <details ref={detailsRef} open={false} className="flex flex-col gap-1">
+    <details
+      ref={detailsRef}
+      open={false}
+      className="flex flex-col gap-1"
+      onClick={handleClick}
+      onDoubleClick={handleDoubleClick}
+      onToggle={toggle}>
       <summary onClick={summaryClick} className={claimStyle} style={{ paddingLeft: paddingLeft }}>
         {children && children.length && (
-          <div className={`p-3 hover:bg-slate-300 rounded-md`} onClick={expandDetails}>
+          <div className={`p-3 hover:bg-slate-300 rounded-md`}>
             <div className={`transition w-full h-full ${detailsOpened ? "rotate-180" : ""}`}>
               <Arrow />
             </div>
@@ -77,26 +129,101 @@ function AccordionComponent({ level, e, parent }: { level: number; e: any; paren
         <Points points={e.points} onNegate={onNegate} />
         <ExternalLink />
       </summary>
-      {children && children.length && <Accordion data={children} level={level + 1} parent={e.title} />}
+      {children && children.length && (
+        <Accordion data={children} level={level + 1} parent={e.title} setHistoricalItems={setHistoricalItems} />
+      )}
     </details>
   );
 }
 
-function Accordion({ data, level, parent }: { data: any; level: number; parent?: string | undefined }) {
-  console.log(data);
+function Accordion({
+  data,
+  level,
+  parent,
+  setHistoricalItems,
+}: {
+  data: any;
+  level: number;
+  parent?: string | undefined;
+  setHistoricalItems: React.Dispatch<React.SetStateAction<string[] | undefined>>;
+}) {
   return (
     <div className="flex flex-col w-full gap-1 ">
       {data?.map((e: any, i: number) => (
-        <AccordionComponent key={i} level={level} e={e} parent={parent} />
+        <AccordionComponent key={i} level={level} e={e} parent={parent} setHistoricalItems={setHistoricalItems} />
       ))}
     </div>
   );
 }
 
-export default function Home() {
+function HistoricalClaims({ claimsIds }: { claimsIds: string[] }) {
+  function findElement(id: string) {
+    if (!id) return items;
+    let item: any;
+
+    const traverse = (_items: any) => {
+      for (const _item of _items) {
+        if (_item.id === id) {
+          item = _item;
+        } else if (_item.children && _item.children.length) {
+          traverse(_item.children);
+        }
+      }
+    };
+
+    traverse(items);
+    return item;
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Accordion data={items} level={0} />
+    <div className="flex flex-col w-full h-fit text-gray-500 mb-4 ">
+      {claimsIds.map((e,i) => (
+        <div key={i} className="relative justify-between items-center gap-4 font-medium cursor-pointer list-none border border-grey-100 -mt-3 bg-white px-5 py-4 rounded-md">{findElement(e).title}</div>
+      ))}
+    </div>
+  );
+}
+
+export default function Home({ searchParams }: { searchParams: { id: string | null } }) {
+  const [filteredItems, setFilteredItems] = useState<any []>();
+  const [historicalItems, setHistoricalItems] = useState<string[]>();
+
+  function findRoot(id: string | undefined | null) {
+    if (!id) return items;
+    let filteredItems: any[] = [];
+    const traverse = (_items: any) => {
+      for (const _item of _items) {
+        console.log(_item.id, id);
+        if (_item.id === id) {
+          console.log("PUSH", _item.id)
+          filteredItems.push(_item);
+          break;
+        } else if (_item.children && _item.children.length) {
+          traverse(_item.children);
+        }
+        if(filteredItems.length !== 0) return
+      }
+    };
+
+    traverse(items);
+    console.log("FILTERED", filteredItems);
+    return filteredItems;
+  }
+
+  useEffect(() => {
+    const param = findRoot(searchParams.id?.split(",")[0])
+    const hist = searchParams.id?.split(",").slice(1)
+    console.log("HIST", hist)
+    setFilteredItems(param);
+    setHistoricalItems(hist);
+  }, [searchParams.id]);
+
+  console.log("HISTORICAL", historicalItems, filteredItems);
+  // let filteredItems = findRoot(searchParams.id);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+      {historicalItems && historicalItems?.length !== 0 && <HistoricalClaims claimsIds={historicalItems.reverse()} />}
+      <Accordion data={filteredItems} level={0} setHistoricalItems={setHistoricalItems} />
     </main>
   );
 }
