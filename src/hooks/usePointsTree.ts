ee4a51dc28428bbcf85c7497f38c65567d372fc0
Connor McCormick <connor@lotspot.co>
2023-09-22 21:39:41 +0200

    Load in points from a farcaster channel

---
+++ src/hooks/usePointsTree.ts
@@ -0,0 +1,57 @@
+// src/hooks/usePointsTree.ts
+import axios from 'axios';
+
+export type PointsTree = {
+  title: string;
+  id: string;
+  points: number;
+  children?: PointsTree[];
+};
+
+function findRoot(id: string | undefined | null, items: any[]) {
+  if (!id) return items;
+  let filteredItems: any[] = [];
+  const traverse = (_items: any) => {
+    for (const _item of _items) {
+      console.log(_item.hash, id);
+      if (_item.hash === id) {
+        console.log("PUSH", _item.hash)
+        filteredItems.push(_item);
+        break;
+      } else if (_item.replies && _item.replies.count > 0) {
+        traverse(_item.replies);
+      }
+      if(filteredItems.length !== 0) return
+    }
+  };
+
+  traverse(items);
+  return filteredItems;
+}
+
+export async function fetchPointsTree(id: string | null) {
+  const options = {
+    method: 'GET',
+    url: 'https://api.neynar.com/v2/farcaster/feed',
+    params: {
+      api_key: process.env.NEYNAR_API_KEY,
+      feed_type: 'filter',
+      filter_type: 'parent_url',
+      parent_url: 'chain://eip155:1/erc721:0xd4498134211baad5846ce70ce04e7c4da78931cc'
+    },
+    headers: {accept: 'application/json', api_key: process.env.NEYNAR_API_KEY}
+  };
+
+  const response = await axios.request(options);
+
+  const pointsTree: PointsTree[] = response.data.casts.map((cast: any) => ({
+    title: cast.text,
+    id: cast.hash,
+    points: cast.reactions.likes.length, // replace with the correct property for points
+  }));
+
+  const param = findRoot(id?.toString().split(",")[0], pointsTree);
+  const hist = id?.toString().split(",").slice(1);
+
+  return { pointsTree: param, historicalItems: hist };
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,38 @@
-import { useEffect, useState } from "react";
-import items from "../data";
+// src/pages/index.tsx
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 import Accordion from "@/components/Accordion";
 import HistoricalClaims from "@/components/HistoricalClaims";
 import Login from "@/components/Login";
-import { useRouter } from 'next/router';
+import { PointsTree, fetchPointsTree } from "@/hooks/usePointsTree";
 
-export default function Home({ searchParams }: { searchParams: { id: string | null } }) {
+export default function Home({ pointsTree, historicalItems }: { pointsTree: PointsTree[], historicalItems: string[] }) {
   const router = useRouter();
-  const { id } = router.query;
+  const id = Array.isArray(router.query.id) ? router.query.id[0] : router.query.id || null;
 
-  const [filteredItems, setFilteredItems] = useState<any []>();
-  const [historicalItems, setHistoricalItems] = useState<string[]>();
-
-  function findRoot(id: string | undefined | null) {
-    if (!id) return items;
-    let filteredItems: any[] = [];
-    const traverse = (_items: any) => {
-      for (const _item of _items) {
-        console.log(_item.id, id);
-        if (_item.id === id) {
-          console.log("PUSH", _item.id)
-          filteredItems.push(_item);
-          break;
-        } else if (_item.children && _item.children.length) {
-          traverse(_item.children);
-        }
-        if(filteredItems.length !== 0) return
-      }
-    };
-
-    traverse(items);
-    return filteredItems;
-  }
-
-  useEffect(() => {
-    const param = findRoot(id?.toString().split(",")[0]);
-    const hist = id?.toString().split(",").slice(1);
-    setFilteredItems(param);
-    setHistoricalItems(hist);
-  }, [id]);
+  // Use the fetched data directly
+  const filteredItems = pointsTree;
+  const [historicalItemsState, setHistoricalItems] = useState<string[] | undefined>(historicalItems);
 
   return (
     <div>
       <header className="flex justify-end m-4"><Login/></header>
       <main className="flex min-h-screen flex-col items-center justify-start p-12 px-48">
-        {historicalItems && historicalItems?.length !== 0 && <HistoricalClaims claimsIds={historicalItems.reverse()} />}
+        {historicalItemsState && historicalItemsState?.length !== 0 && <HistoricalClaims claimsIds={historicalItemsState.reverse()} />}
         <Accordion data={filteredItems} level={0} setHistoricalItems={setHistoricalItems} />
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const id = null; // or replace with an appropriate id value
+  const { pointsTree, historicalItems = [] } = await fetchPointsTree(id);
+
+  return {
+    props: {
+      pointsTree,
+      historicalItems,
+    },
+  };
 }
