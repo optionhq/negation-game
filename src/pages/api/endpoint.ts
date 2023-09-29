8fd5f8df2a2864155a253cd77582130212f5b96c
Connor McCormick <connor@lotspot.co>
2023-09-29 17:12:36 +0200

    Video for marie

--- src/pages/api/endpoint.ts
+++ src/pages/api/endpoint.ts
@@ -3,17 +3,15 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { extractEndPointUrl, getEndPoint } from '@/hooks/useEndPoint';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { cast } = req.query;
-  if (!cast) {
-    return res.status(400).json({ error: 'Missing cast parameter' });
+  const { endPointUrl } = req.query;
+  if (!endPointUrl) {
+    return res.status(400).json({ error: 'Missing endPointUrl parameter' });
   }
 
   try {
-    const endPointUrl = extractEndPointUrl(cast);
-    if (endPointUrl) {
       const endPoint = await getEndPoint(endPointUrl);
       return res.status(200).json(endPoint);
-    }
+  }
   } catch (error) {
     console.error('Error fetching endpoint:', error);
     return res.status(500).json({ error: 'Error fetching endpoint' });
