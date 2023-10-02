2b21c70ddf481acdd0a247a4e3f216e0e6309943
Connor McCormick <connor@lotspot.co>
2023-10-02 15:17:47 +0200

    Fix bug in endpoint.ts

--- src/pages/api/endpoint.ts
+++ src/pages/api/endpoint.ts
@@ -1,17 +1,22 @@
 // pages/api/endpoint.ts
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { extractEndPointUrl, getEndPoint } from '@/hooks/useEndPoint';
+import { getEndPoint } from '@/hooks/useEndPoint';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { endPointUrl } = req.query;
+  let { endPointUrl } = req.query;
+  
+  // Ensure endPointUrl is a string
+  if (Array.isArray(endPointUrl)) {
+    endPointUrl = endPointUrl[0];
+  }
+
   if (!endPointUrl) {
     return res.status(400).json({ error: 'Missing endPointUrl parameter' });
   }
 
   try {
-      const endPoint = await getEndPoint(endPointUrl);
-      return res.status(200).json(endPoint);
-  }
+    const endPoint = await getEndPoint(endPointUrl);
+    return res.status(200).json(endPoint);
   } catch (error) {
     console.error('Error fetching endpoint:', error);
     return res.status(500).json({ error: 'Error fetching endpoint' });
