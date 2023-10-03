ad2f55f4ab7e3042c5c006a97165c396904ea1a0
Connor McCormick <connor@lotspot.co>
2023-10-03 15:34:11 +0200

    Remove SimpleComponent

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,6 @@ import Login from "@/components/Login";
 import { fetchPointsTree } from "@/hooks/usePointsTree";
 import { LinkPointsTree } from '@/types/PointsTree';
 import { GetServerSidePropsContext } from 'next';
-import SimpleComponent from '@/components/SimpleComponent';
 
 export default function Home({ pointsTree, historicalItems }: { pointsTree: LinkPointsTree[], historicalItems: string[] }) {
   const router = useRouter();
@@ -20,7 +19,6 @@ export default function Home({ pointsTree, historicalItems }: { pointsTree: Link
   return (
     <div>
       <header className="flex justify-end m-4"><Login/></header>
-      < SimpleComponent />
       <main className="flex min-h-screen flex-col items-center justify-start p-12 px-48">
         {historicalItemsState && historicalItemsState?.length !== 0 && <HistoricalClaims claimsIds={historicalItemsState.reverse()} />}
         <Accordion data={filteredItems} level={0} setHistoricalItems={setHistoricalItems} />
