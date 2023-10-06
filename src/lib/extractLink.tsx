6f589bf6561381f39860755fe8c045a06304b104
mgavillo <marie.gavillon@gmail.com>
2023-10-06 13:13:44 +0200

    error fix

--- src/lib/extractLink.tsx
+++ src/lib/extractLink.tsx
@@ -1,4 +1,5 @@
-export function extractLink(text: string): { text: string, link: string | null } {
+export function extractLink(text: string): { text: string; link: string | null } {
+  if (!text) return { text: "", link: null };
   // Regular expression to match links starting with "https://warpcast.com/"
   const linkRegex = /(https:\/\/warpcast\.com\/[^\s]+)/;
 
@@ -8,14 +9,14 @@ export function extractLink(text: string): { text: string, link: string | null }
   if (match && match.length > 0) {
     // Extract the link
     const link = match[0];
-    
+
     // Check if the link is at the end of the text
     if (text.endsWith(link)) {
       // Extract the text before the link
       const extractedText = text.substring(0, text.length - link.length);
       return {
         text: extractedText.trim(),
-        link: link
+        link: link,
       };
     }
   }
@@ -23,6 +24,6 @@ export function extractLink(text: string): { text: string, link: string | null }
   // If no matching link is found at the end of the text, return link as null
   return {
     text: text,
-    link: null
+    link: null,
   };
 }
