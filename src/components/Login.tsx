295c5486c73950a86a44144001eb6da75acc703a
mgavillo <marie.gavillon@gmail.com>
2023-10-08 09:38:30 +0100

    fix button styling

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
-import axios from 'axios';
-import { useRouter } from 'next/router';
-import { LOCAL_STORAGE_KEYS } from './constants';
-import Link from 'next/link';
-import { FarcasterUser } from '@/types/FarcasterUser';
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { LOCAL_STORAGE_KEYS } from "./constants";
+import Link from "next/link";
+import { FarcasterUser } from "@/types/FarcasterUser";
 
 function Login() {
   const [loading, setLoading] = useState(false);
@@ -20,8 +20,8 @@ function Login() {
   }, []);
 
   useEffect(() => {
-    if (farcasterUser && farcasterUser.status === 'pending_approval') {
-      router.push('/qr-code');
+    if (farcasterUser && farcasterUser.status === "pending_approval") {
+      router.push("/qr-code");
     }
   }, [farcasterUser, router]);
 
@@ -29,17 +29,22 @@ function Login() {
     <div>
       {!farcasterUser?.status && (
         <button
-          style={{ backgroundColor: 'purple', color: 'white', padding: '10px 20px', fontSize: '16px', cursor: loading ? 'not-allowed' : 'pointer' }}
+          className="button"
+          style={{ cursor: loading ? "not-allowed" : "pointer" }}
           onClick={handleSignIn}
-          disabled={loading}
-        >
-          {loading ? 'Loading...' : 'Sign in with farcaster'}
+          disabled={loading}>
+          {loading ? "Loading..." : "Sign in with farcaster"}
         </button>
       )}
-      {farcasterUser?.status === 'approved' && (
+      {farcasterUser?.status === "approved" && (
         <div>
           {/* Display user's profile picture and username here */}
-          <Image src={farcasterUser.profile_picture || "/default-avatar.svg"} alt="Description" width={50} height={50} />
+          <Image
+            src={farcasterUser.profile_picture || "/default-avatar.svg"}
+            alt="Description"
+            width={50}
+            height={50}
+          />
           <span>{farcasterUser.username}</span>
         </div>
       )}
@@ -54,13 +59,13 @@ function Login() {
 
   async function createAndStoreSigner() {
     try {
-      const response = await axios.post('/api/signer');
+      const response = await axios.post("/api/signer");
       if (response.status === 200) {
         localStorage.setItem(LOCAL_STORAGE_KEYS.FARCASTER_USER, JSON.stringify(response.data));
         setFarcasterUser(response.data);
       }
     } catch (error) {
-      console.error('API Call failed', error);
+      console.error("API Call failed", error);
     }
   }
 }
