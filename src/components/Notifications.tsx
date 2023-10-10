b32efa1d2d695f839ab3363016f199d46e870ba2
mgavillo <marie.gavillon@gmail.com>
2023-10-10 15:44:52 +0100

    changed notification to be + when more than 10

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -6,7 +6,7 @@ export default function Notifications({ notifNb = 3 }: { notifNb?: number }) {
     <Tooltip orientation="top" text="Notifications">
       <button className="relative flex items-center h-full justify-center border rounded-md bg-slate-100 hover:bg-slate-200 px-2">
         {notifNb > 0 && <MdNotifications size={24} />}
-        <div className="absolute -right-1 -top-1 bg-red-500 rounded-full py-1 px-2 text-xs text-white">{notifNb}</div>
+        <div className="absolute -right-1 -top-1 bg-red-500 rounded-full py-1 px-2 text-xs text-white">{notifNb < 10 ? notifNb : "+"}</div>
       </button>
     </Tooltip>
   );
