38131a63ae68621f278595c591edb1e473dcfa73
mgavillo <marie.gavillon@gmail.com>
2023-10-10 15:08:54 +0100

    notifications

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,41 @@
 // src/pages/index.tsx
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import Accordion from "@/components/Accordion";
 import HistoricalClaims from "@/components/HistoricalClaims";
 import Login from "@/components/Login";
-import { LinkPointsTree } from '@/types/PointsTree';
-import Cast from '@/components/Cast';
-import { LOCAL_STORAGE_KEYS } from '@/components/constants';
-import { FarcasterUser } from '@/types/FarcasterUser';
-import axios from 'axios';
-import { NextPageContext } from 'next';
-import { fetchFeed } from '@/pages/api/feed';
-import { FarcasterUserContext } from '@/contexts/UserContext';
+import { LinkPointsTree } from "@/types/PointsTree";
+import Cast from "@/components/Cast";
+import { LOCAL_STORAGE_KEYS } from "@/components/constants";
+import { FarcasterUser } from "@/types/FarcasterUser";
+import axios from "axios";
+import { NextPageContext } from "next";
+import { fetchFeed } from "@/pages/api/feed";
+import { FarcasterUserContext } from "@/contexts/UserContext";
+import Notifications from "@/components/Notifications";
 
-export default function Home({ initialPointsTree, initialHistoricalItems }: { initialPointsTree: LinkPointsTree[], initialHistoricalItems: string[] }) {
+export default function Home({
+  initialPointsTree,
+  initialHistoricalItems,
+}: {
+  initialPointsTree: LinkPointsTree[];
+  initialHistoricalItems: string[];
+}) {
   const router = useRouter();
   const id = Array.isArray(router.query.id) ? router.query.id[0] : router.query.id || null;
 
   const [filteredItems, setFilteredItems] = useState(initialPointsTree);
   const [historicalItemsState, setHistoricalItems] = useState<string[] | undefined>(initialHistoricalItems);
-  
+
   const [farcasterUser, setFarcasterUser] = useState<FarcasterUser | null>(null);
 
   const reloadThreads = async () => {
     try {
       const response = await axios.get(`/api/feed?id=${router.query.id}`);
-      console.log('reload response', response.data)
+      console.log("reload response", response.data);
       setFilteredItems(response.data.pointsTree);
     } catch (error) {
-      console.error('Could not reload threads', error);
+      console.error("Could not reload threads", error);
     }
   };
 
@@ -42,10 +49,15 @@ export default function Home({ initialPointsTree, initialHistoricalItems }: { in
 
   return (
     <div>
-      <header className="flex justify-end m-4"><Login/></header>
-      <main className="flex min-h-screen flex-col items-center justify-start p-12 px-48">
-        {farcasterUser?.status == 'approved' && <Cast farcasterUser={farcasterUser} reloadThreads={reloadThreads}/>}
-        {historicalItemsState && historicalItemsState?.length !== 0 && <HistoricalClaims claimsIds={historicalItemsState.reverse()} />}
+      <header className="flex justify-end px-4 py-2 gap-2 bg-white border fixed top-0 w-screen z-40">
+        <Notifications/>
+        <Login />
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-start p-12 pt-24 px-48">
+        {farcasterUser?.status == "approved" && <Cast farcasterUser={farcasterUser} reloadThreads={reloadThreads} />}
+        {historicalItemsState && historicalItemsState?.length !== 0 && (
+          <HistoricalClaims claimsIds={historicalItemsState.reverse()} />
+        )}
         <FarcasterUserContext.Provider value={{ farcasterUser, setFarcasterUser }}>
           <Accordion data={filteredItems} level={0} setHistoricalItems={setHistoricalItems} />
         </FarcasterUserContext.Provider>
@@ -57,7 +69,7 @@ export default function Home({ initialPointsTree, initialHistoricalItems }: { in
 Home.getInitialProps = async (context: NextPageContext) => {
   let id = context.query.threadId || null;
 
-  const { pointsTree, historicalItems } = await fetchFeed(id as string)
+  const { pointsTree, historicalItems } = await fetchFeed(id as string);
 
   return {
     initialPointsTree: pointsTree,
