a88f65d05c8b6d9a1e61a9e436619c1b3a6a776e
mgavillo <marie.gavillon@gmail.com>
2023-10-10 15:49:05 +0100

    changed again notification to be only 99+ when over 100

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,12 +1,12 @@
 import { MdNotifications } from "react-icons/md";
 import Tooltip from "./Tooltip";
 
-export default function Notifications({ notifNb = 3 }: { notifNb?: number }) {
+export default function Notifications({ notifNb = 100 }: { notifNb?: number }) {
   return (
     <Tooltip orientation="top" text="Notifications">
       <button className="relative flex items-center h-full justify-center border rounded-md bg-slate-100 hover:bg-slate-200 px-2">
         {notifNb > 0 && <MdNotifications size={24} />}
-        <div className="absolute -right-1 -top-1 bg-red-500 rounded-full py-1 px-2 text-xs text-white">{notifNb < 10 ? notifNb : "+"}</div>
+        <div className="absolute left-full -translate-x-1/2 -top-1 bg-red-500 rounded-full py-1 px-2 text-xs text-white">{notifNb < 100 ? notifNb : "99+"}</div>
       </button>
     </Tooltip>
   );
