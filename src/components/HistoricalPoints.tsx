cc75b7bb29aa749c200a20fd8262cdefb056019b
Connor McCormick <connor@lotspot.co>
2023-11-10 12:59:54 -0700

    Revert bug: clicking historical points didn't keep them in the right order

--- src/components/HistoricalPoints.tsx
+++ src/components/HistoricalPoints.tsx
@@ -2,17 +2,19 @@ import { BiChevronLeft } from 'react-icons/bi';
 import Point from './Point'
 import { useRouter } from 'next/router';
 
-export default function HistoricalPoints({ ids, _onClick }: { ids: string[], _onClick?: (id: string) => void }) {
+export default function HistoricalPoints({ ids }: { ids: string[] } ) {
   const router = useRouter();
 
   function onClick(id: string) {
-    const index = ids.indexOf(id);
-    const newIds = ids.slice(index);
+    const reverseIds = ids.reverse()
+    const index = reverseIds.indexOf(id);
+    const newIds = reverseIds.slice(index);
     router.push({
       pathname: router.pathname,
       query: { ...router.query, id: newIds.join(',') },
     });
   }
+
 
   return (
     <div className="flex flex-col h-fit text-gray-500 space-y-0 gap-1 centered-element">
