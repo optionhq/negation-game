f1a73412cc4303bf3c1a4b7b92bbff40deea6322
Connor McCormick <connor@lotspot.co>
2023-11-27 20:44:21 -0700

    Make history prettier

--- src/components/HistoricalPoints.tsx
+++ src/components/HistoricalPoints.tsx
@@ -1,6 +1,37 @@
-import { BiChevronLeft } from 'react-icons/bi';
-import Point from './Point'
 import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import axios from "axios";
+import { Cast } from "neynar-next/server";
+import { useState } from "react";
+import { getMaybeNegation } from "@/lib/useCasts";
+import { Point, Negation } from "@/types/Points";
+
+function HistoricalPoint({ id, onClick }: { id: string, onClick: () => void }) {
+
+  const [cast, setCast] = useState<Negation | null>(null);
+
+  useEffect(() => {
+    const fetchCast = async () => {
+      const res = await axios.get(`/api/cast?type=hash&identifier=${id}`)
+      const cast: Cast = res.data
+      const maybeNegation = await getMaybeNegation(cast)
+      setCast(maybeNegation)
+    };
+    fetchCast();
+  }, [id]);
+
+  if (!cast)
+    return <p> Loading... </p>
+  return <>
+    <p className='flex items-center p-7 text-gray-500'>{cast.advocates.length}</p>
+    <p
+      onClick={onClick}
+      className="min-h-[70px] font-medium text-gray-900 flex items-center"
+    >
+      {cast.endPoint ? cast.endPoint.title : cast.title}
+    </p>
+  </>
+}
 
 export default function HistoricalPoints({ ids }: { ids: string[] } ) {
   const router = useRouter();
@@ -17,9 +48,11 @@ export default function HistoricalPoints({ ids }: { ids: string[] } ) {
 
 
   return (
-    <div className="flex flex-col h-fit text-gray-500 space-y-0 gap-1 centered-element">
+    <div className="flex flex-col h-fit space-y-0 gap-1 pb-1 centered-element">
       {ids.map((id, i) => (
-        <Point key={id} id={id} onClick={() => onClick(id)} />
+        <div className="flex cursor-pointer rounded-md border border-grey-100 hover:bg-gray-100">
+          <HistoricalPoint key={id} id={id} onClick={() => onClick(id)} />
+        </div>
       ))}
     </div>
   );
