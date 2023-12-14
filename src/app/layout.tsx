83d59a4037269e88b64ad51b700db744a5830520
mgavillo <marie.gavillon@gmail.com>
2023-12-14 22:31:51 +0000

    rename

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 import type { Metadata } from 'next';
+import MobileBottomHeader from '@/components/header/MobileBottomHeader';
+import { NeynarProvider } from 'neynar-next';
+import Header from '@/components/header/Header';
 
 export const metadata: Metadata = {
   title: 'Negation Game',
@@ -17,13 +20,18 @@ export default function RootLayout({
     <main className={inter.className}>
       <head>
         <title>Negation Game</title>
-        <meta name="description" content="How extraterrestrials do governance."/>
+        <meta name="description" content="How extraterrestrials do governance." />
       </head>
-      {/* <Head>
-        <title>{metadata.title as string}</title>
-        <meta name="description" content={metadata.description as string} />
-      </Head> */}
-      {children}
+      <NeynarProvider>
+        <div className='flex flex-col min-h-screen'>
+          <Header />
+          <div className='flex-1'>
+            {children}
+
+          </div>
+          <MobileBottomHeader />
+        </div>
+      </NeynarProvider>
     </main>
   );
 }
