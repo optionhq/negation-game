47b4e763636efa575c1eb2a0c8c6f868f12d8eaa
mgavillo <marie.gavillon@gmail.com>
2023-12-15 00:19:03 +0000

    remove test title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import Home from "@/components/Home"
-import Head from "next/head"
 
 export default function HomePage() {
     return (<div>
-        <Head >
-            <title>TEST</title>
-        </Head>
         <Home />
     </div>)
 }
