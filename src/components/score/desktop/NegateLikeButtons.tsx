ea6a0583a0721cbd892eedbaee68c620652602b9
Connor McCormick <connor@lotspot.co>
2023-12-20 11:10:15 -0700

    Fix mistaken tooltips

--- src/components/score/desktop/NegateLikeButtons.tsx
+++ src/components/score/desktop/NegateLikeButtons.tsx
@@ -9,12 +9,12 @@ export default function NegateLikeButtons({ type }: { type: "relevance" | "verac
     
     return (
         <div className=" hidden group-hover/points:flex flex-row gap-1">
-            <Tooltip text={liked?.[type] ? (type == "veracity" ? "Unmark good" : "Unmark correct") : (type == "veracity" ? "Good" : "Correct")} orientation="bottom">
+            <Tooltip text={liked?.[type] ? (type == "veracity" ? "Unmark important" : "Unmark correct") : (type == "veracity" ? "Important" : "Correct")} orientation="bottom">
                 <button className={liked?.[type] ? "text-green-500 text-xl" : "hover:text-green-500 text-xl"} onMouseDown={(e) => handleLike(e, type)}>
                     <HiOutlineCheckCircle size={24} />
                 </button>
             </Tooltip>
-            <Tooltip text={type == "veracity" ? "veracity" : "relevance"} orientation="bottom">
+            <Tooltip text={type == "veracity" ? "Alternative" : "Counterpoint"} orientation="bottom">
                 <button
                     className="hover:text-purple-600 text-xl"
                     onMouseDown={(e) => handleNegate(e, type)}>
