82a6a12e5da508e768808037b0dd06b3fe77fdb4
mgavillo <marie.gavillon@gmail.com>
2024-01-16 13:41:26 +0000

    notification text as span

--- src/pages/notifications/index.tsx
+++ src/pages/notifications/index.tsx
@@ -57,10 +57,10 @@ function ReplyNotification({ notification }: { notification: any }) {
             {/* {parent?.type == "root" && node?.type == "negation"  && } */}
 
             <div className="flex flex-col gap-3">
-                <div className="flex flex-row gap-1">
+                <p className="inline-block">
                     <a className=" font-semibold hover:underline" href={"https://warpcast.com/" + node?.author?.username} target="_blank" >{node?.author?.username}</a>
-                    {node?.type == "negation" ? <p> negated {parent?.type == "negation" ? "the importance" : "the accuracy"} of your point.</p> : <p>replied to your point.</p>}
-                </div>
+                    {node?.type == "negation" ? ` negated ${parent?.type == "negation" ? "the importance" : "the accuracy"} of your point.` : " replied to your point."}
+                </p>
                 <div className="table table-fixed w-full overflow-hidden">
                     <div className="flex flex-col gap-2 ">
                         <p className="text-sm text-black/50">{parent?.type == "negation" ? parent.endPoint!.title : parent?.title}</p>
@@ -90,13 +90,13 @@ function MentionNotification({ notification }: { notification: any }) {
 
 function LikersTooltip({ reactions }: { reactions: any }) {
     return (
-        <div className="absolute flex flex-col top-0 bg-slate-50 border rounded-md p-2 w-44" style={{ transform: "translateY(-100%)" }}>
+        <span className="absolute flex flex-col top-0 bg-slate-50 border rounded-md p-2 w-44 left-0" style={{ transform: "translateY(-100%)", marginTop: "-4px"}}>
             {reactions.slice(1).map((reaction: any) => (
-                <div key={reaction.user.username}>
+                <span key={reaction.user.username}>
                     <a href={"https://warpcast.com/" + reaction.user.username} className="font-semibold hover:underline" target="_blank">{reaction.user.username}</a>
-                </div>
+                </span>
             ))}
-        </div>
+        </span>
     )
 }
 
@@ -123,20 +123,22 @@ function LikeNotification({ notification }: { notification: any }) {
     return (
         <div className="flex flex-row gap-3 ">
             <LikeIcon color={node?.type == "negation" ? "#6b21a8" : "#1e40af"} />
-            {/* {node?.type == "negation" ? <ImportanceIcon /> : <AccuracyIcon />} */}
             <div >
-                <div className="flex flex-row gap-1 flex-wrap">
-                    <a className=" font-semibold hover:underline" href={"https://warpcast.com/" + notification.reactions[0].user.username} target="_blank" >{notification.reactions[0].user.username}</a>
+                <p className="">
+                    <span className="font-semibold hover:underline">
+                        <a href={"https://warpcast.com/" + notification.reactions[0].user.username} target="_blank">{notification.reactions[0].user.username}</a>
+                    </span>
                     {likersLen > 1 &&
-                        <div className="flex flex-row gap-1 ">
-                            <p>and</p>
-                            <div className="relative w-fit" onPointerEnter={() => setShowTooltip(true)} onPointerLeave={() => setShowTooltip(false)}>
-                                <p className="font-semibold hover:underline w-fit">{likersLen - 1} other{likersLen > 2 ? 's' : ''}</p>
-                                {showTooltip && <div><LikersTooltip reactions={notification.reactions} /></div>}
-                            </div>
-                        </div>}
-                    <p>think{thirdPerson} your point is {isLinkingPoint ? 'important' : 'accurate'}.</p>
-                </div>
+                        <>
+                            <span> and </span>
+                            <span className="relative w-fit" onPointerEnter={() => setShowTooltip(true)} onPointerLeave={() => setShowTooltip(false)}>
+                                <span className="font-semibold hover:underline w-fit">{likersLen - 1} other{likersLen > 2 ? 's' : ''}</span>
+                                {showTooltip && <LikersTooltip reactions={notification.reactions} />}
+                            </span>
+                        </>
+                    }
+                    <span> think{thirdPerson} your point is {isLinkingPoint ? 'important' : 'accurate'}.</span>
+                </p>
                 <div className="table table-fixed w-full overflow-hidden">
                     <p className=" text-black/50 text-sm ">{isLinkingPoint ? node?.endPoint!.title : node?.title}</p>
                 </div>
@@ -167,7 +169,7 @@ function Notification({ notification, previousNotif }: { notification: any, prev
     const replyHistoric = notification.type == "reply" ? `%2C0x${node?.parentId}` : ""
     if (notification.type !== "follows" && notification.type !== "mention" && notification.type !== "likes" && notification.type !== "reply") console.log(notification)
     return (
-        <div className={`border p-4 w-[500px] rounded-md hover:bg-slate-50 cursor-pointer ${isNew ? "bg-indigo-600/10" : ""}`} onClick={() => node && router.push(`/?id=${node.id}${replyHistoric}`)}>
+        <div className={`border p-4 w-full md:w-[500px] rounded-md hover:bg-slate-50 cursor-pointer ${isNew ? "bg-indigo-600/10" : ""}`} onClick={() => node && router.push(`/?id=${node.id}${replyHistoric}`)}>
             {notification.type == "follows" && <FollowNotification notification={notification} />}
             {notification.type == "mention" && <MentionNotification notification={notification} />}
             {notification.type == "likes" && <LikeNotification notification={notification} />}
@@ -199,7 +201,7 @@ export default function Notifications() {
         localStorage.setItem("old_most_recent_notification", old_most_recent)
         const result: any[] | undefined = await getNotifications(signer, NB_NOTIF)
         if (!result || !result[0].length) return
-        
+
         const [_notifications, nextCursor] = result
         localStorage.setItem("most_recent_notification", _notifications[0].most_recent_timestamp)
         cursor.current = nextCursor
@@ -223,7 +225,7 @@ export default function Notifications() {
 
     console.log(notifications)
     return (
-        <div className="flex-1 py-12 flex justify-center">
+        <div className="flex-1 py-6 sm:py-12 flex justify-center mx-4 md:mx-12 ">
             <div className="flex flex-col items-center gap-2">
                 {notifications.map((notif, i) => <Notification notification={notif} key={i} previousNotif={previousNotif} />)}
                 <div className="loader" ref={loader}></div>
