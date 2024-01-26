e1f0e9e7b17d64e34a40bd3939fe2f3aba7a84b2
Volky <volkyeth@pm.me>
2024-01-26 15:32:23 -0300

    Add graph loader

--- src/app/graph/page.tsx
+++ src/app/graph/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 import { Graph } from "@/components/Graph";
 import { fetchGraph } from "@/lib/actions/fetchGraph";
+import { BiLoaderAlt } from "react-icons/bi";
 import useSWR from "swr";
 
 export default function Page() {
 	const { data } = useSWR("graph", () => fetchGraph());
 
 	return (
-		<div className="w-screen h-screen">
+		<div className="w-full h-full flex items-center justify-center">
+			{!data && (
+				<BiLoaderAlt size={128} className="animate-spin text-purple-200" />
+			)}
 			{data && <Graph className="w-full h-full bg-gray-100" elements={data} />}
 		</div>
 	);
