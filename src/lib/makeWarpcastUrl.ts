fd0ea0246ed2184ac88d7cb334c01f80f1ddbe3b
Volky <volkyeth@pm.me>
2024-02-01 16:31:14 -0300

    Fix warpcast url

--- src/lib/makeWarpcastUrl.ts
+++ src/lib/makeWarpcastUrl.ts
@@ -2,5 +2,5 @@ import { Node } from "../types/Points";
 
 export default function makeWarpcastUrl(cast: Node) {
 	if (cast.id)
-		return (`https://warpcast.com/${cast.author?.username}/cast.id.slice(0, 8).toString()`);
+		return `https://warpcast.com/${cast.author?.username}/${cast.id}`;
 }
