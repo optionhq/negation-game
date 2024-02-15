ef25db3cbf11130b78f3758418cd578b14f97815
Volky <volkyeth@pm.me>
2024-02-15 15:22:05 -0300

    Prevent hidden footer on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { CytoscapeProvider } from "@/contexts/CytoscapeContext";
 import { SignerProvider } from "@/contexts/SignerContext";
 import "@/globals.css";
 import { cn } from "@/lib/utils";
-import { Metadata } from "next";
+import { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import Link from "next/link";
 import { IoArrowRedo } from "react-icons/io5";
@@ -24,6 +24,13 @@ export const metadata: Metadata = {
 	},
 };
 
+export const viewport: Viewport = {
+	width: "device-width",
+	initialScale: 1.0,
+	height: "device-height",
+	interactiveWidget: "resizes-content",
+};
+
 export default function RootLayout({
 	children,
 }: {
@@ -31,10 +38,10 @@ export default function RootLayout({
 }) {
 	return (
 		<CytoscapeProvider>
-			<html lang="en" className="h-full overscroll-contain">
-				<body className={cn("h-full  font-sans", inter.variable)}>
+			<html lang="en" className="h-[100dvh] overscroll-contain">
+				<body className={cn("h-[100dvh]  font-sans", inter.variable)}>
 					<SignerProvider>
-						<div className="flex h-full min-h-screen flex-col ">
+						<div className="flex h-[100dvh] min-h-[100dvh] flex-col ">
 							<header className="md: sticky top-0 z-50 grid w-full grid-cols-2 items-center justify-center gap-6 border bg-slate-50 px-4 py-2 md:grid-cols-[auto_1fr_auto]">
 								<Link
 									href={"/"}
